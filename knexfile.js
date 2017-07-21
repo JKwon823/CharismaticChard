36065921135644a9ec130f1d3112872adb51f5d1
Joseph Kwon <kwonjosepho@gmail.com>
2017-07-20 20:31:47 -0700

    (bug) fix knexfile bug for heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,14 @@
 const config = require('config');
 
-module.exports = config['knex'];
+module.exports = config['knex'] || {
+  connection: {
+    database: 'process.env.DATABASE_URL',
+    user: 'process.env.Heroku_user',
+    password: 'process.env.Heroku_password'
+  },
+  debug: false,
+  pool: {
+    min: 2,
+    max: 10
+  }
+};
