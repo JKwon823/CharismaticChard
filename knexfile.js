bab2beafe8d7af49356deefb877ca452ed9baaa0
Joseph Kwon <kwonjosepho@gmail.com>
2017-07-20 20:59:32 -0700

    (bug) fix knexfile bug for heroku deployment

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,18 @@
 const config = require('config');
 
-module.exports = config['knex'] || {
+module.exports = {
   client: 'pg',
   connection: process.env.DATABASE_URL,
   debug: false,
   pool: {
     min: 2,
     max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: 'db/migrations'
+  },
+  seeds: {
+    directory: 'db/seeds'
   }
 };
