8cdc9a392e3b8398ba3bd52583468431a4f4e199
Joseph Kwon <kwonjosepho@gmail.com>
2017-07-24 15:03:09 -0700

    (bug) changing a require statement path to see if conditional is working as intended

--- server/services/twilio/twilio.js
+++ server/services/twilio/twilio.js
@@ -7,7 +7,7 @@ if (process.env.Twilio_accountSid && process.env.Twilio_authToken) {
 } else if (path.isAbsolute('./config')) {
   var config = require('./config.js');
 } else {
-  var config = require('./config_example.js');
+  var config = require('./config.js');
 }
 var twilio = require('twilio');
 var client = new twilio(config.accountSid, config.authToken);
