23bbd44a482d162df1c8a2cb74f4fc537ba20573
Joseph Kwon <kwonjosepho@gmail.com>
2017-07-29 10:42:51 -0700

    (feat) add new files and revert files to be deleted

--- client/src/reducers/outputReducer.js
+++ client/src/reducers/outputReducer.js
@@ -1,17 +1,14 @@
-export default function reducer(state = {
-  debtors: null,
-  friendsInfo: [],
-  users: []
-}, action) {
+export default function reducer(state =
+  {
+    debtors: null,
+    friendsInfo: []
+  }, action) {
   switch (action.type) {
   case 'SET_DEBTORS': {
     return {...state, debtors: action.payload};
   }
   case 'SET_FRIENDSINFO': {
     return {...state, friendsInfo: [...state.friendsInfo, action.payload]};
-  }
-  case 'GET_USERS': {
-    return {...state, users: action.payload};
   }
   default: {
     return state;
