c44574ddfafbce5f042cf10906280d0bb8f597bf
Joseph Kwon <kwonjosepho@gmail.com>
2017-08-02 15:21:06 -0700

    (feat) make addFriends button have valid phone number check functionality

--- client/src/components/addFriends.js
+++ client/src/components/addFriends.js
@@ -28,7 +28,8 @@ export class AddFriends extends React.Component {
       showModal: false,
       name: null,
       number: null,
-      validationState: null
+      validationState: null,
+      isValid: null
     };
   }
 
@@ -57,11 +58,11 @@ export class AddFriends extends React.Component {
   }
 
   friendNumber(e) {
-    let temp = phoneUtil.parse(e.target.value, 'US');
-    console.log(phoneUtil.parse(e.target.value, 'US'));
+    e.target.value = phoneUtil.format(phoneUtil.parse(e.target.value, 'US'), PhoneNumberFormat.NATIONAL);
     this.setState({
       number: e.target.value,
-      validationState: null
+      validationState: null,
+      isValid: phoneUtil.isValidNumber(phoneUtil.parse(e.target.value, 'US'))
     });
   }
 
@@ -81,45 +82,38 @@ export class AddFriends extends React.Component {
 
   render() {
     return (
-      <div className="row text-center">
-        <div className="col-xs-12">
-          <div className="row">
-            <Button id="add-friends" bsStyle="primary" bsSize="small" onClick={this.toggle.bind(this)}>
-              Add Friends
-            </Button>
-          </div>
-          <AddFriendsByUserButton />
-          <div className="row">
-            <Modal show={this.state.showModal} onHide={this.toggle.bind(this)}>
-              <Modal.Header closeButton>
-                <Modal.Title>Add a friend</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <Form horizontal onSubmit={this.toggle.bind(this)}>
-                  <FormGroup controlId="formName">
-                    <Col componentClass={ControlLabel} sm={2}>
-                      Name
-                    </Col>
-                    <Col sm={10}>
-                      <FormControl type="email" placeholder="Friend's Name" onChange={this.friendName.bind(this)}/>
-                    </Col>
-                  </FormGroup>
-                  <FormGroup controlId="formPhoneNumber" validationState={this.state.validationState}>
-                    <Col componentClass={ControlLabel} sm={2}>
-                      Number
-                    </Col>
-                    <Col sm={10}>
-                      <FormControl type="email" placeholder="xxx-xxx-xxxx" onChange={this.friendNumber.bind(this)} value={this.state.number}/>
-                    </Col>
-                  </FormGroup>
-                </Form>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button bsStyle="primary" onClick={this.handleAdd.bind(this)}>ADD</Button>
-              </Modal.Footer>
-            </Modal>
-          </div>
-        </div>
+      <div className="text-center">
+        <Button id="add-friends" bsStyle="primary" bsSize="small" onClick={this.toggle.bind(this)}>
+          Add Friends
+        </Button>
+        <Modal show={this.state.showModal} onHide={this.toggle.bind(this)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add a friend</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form horizontal onSubmit={this.toggle.bind(this)}>
+              <FormGroup controlId="formName">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Name
+                </Col>
+                <Col sm={10}>
+                  <FormControl type="text" placeholder="Friend's Name" onChange={this.friendName.bind(this)}/>
+                </Col>
+              </FormGroup>
+              <FormGroup controlId="formPhoneNumber" validationState={this.state.validationState}>
+                <Col componentClass={ControlLabel} sm={2}>
+                  Number
+                </Col>
+                <Col sm={10}>
+                  <FormControl type="text" placeholder="(xxx)xxx-xxxx" onChange={this.friendNumber.bind(this)}/>
+                </Col>
+              </FormGroup>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle="primary" onClick={this.state.isValid ? this.handleAdd.bind(this) : this.handleFailAdd.bind(this)}>ADD</Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
